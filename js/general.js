833b3bed3900303e5df060c292a1c7be6c3cbad6
Andrea Szabó <96067848+ReaSzabo@users.noreply.github.com>
2026-03-01 18:46:39 +0100

    categories

--- js/general.js
+++ js/general.js
@@ -26,5 +26,41 @@ $(document).ready(function() {
   if (middleHeight < windowHeight)
   {
     $('#middle').css('min-height', windowHeight);
-  }                    
+  }
+  
+  // Dropdown menu kattintás kezelés
+  $('#menu .dropdown > a').on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    
+    var $dropdown = $(this).parent('.dropdown');
+    var $dropdownContent = $dropdown.find('.dropdown-content');
+    
+    // Toggle dropdown
+    if ($dropdownContent.hasClass('show')) {
+      $dropdownContent.removeClass('show');
+      $(this).removeClass('active');
+    } else {
+      // Close all other dropdowns
+      $('.dropdown-content.show').removeClass('show');
+      $('#menu .dropdown > a.active').removeClass('active');
+      
+      // Open this dropdown
+      $dropdownContent.addClass('show');
+      $(this).addClass('active');
+    }
+  });
+  
+  // Bezárás ha máshová kattintunk
+  $(document).on('click', function(e) {
+    if (!$(e.target).closest('#menu .dropdown').length) {
+      $('.dropdown-content.show').removeClass('show');
+      $('#menu .dropdown > a.active').removeClass('active');
+    }
+  });
+  
+  // Ne záródjon be ha a dropdown-ra kattintunk
+  $('#menu .dropdown-content').on('click', function(e) {
+    e.stopPropagation();
+  });
 });
